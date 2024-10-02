5445d303c4656fc7ea7a95620adf9f95cab70fe2
zb656 <145730190+zb656@users.noreply.github.com>
2024-10-02 16:47:34 -0400

    cant figure out different colors

--- A/sketch.js
+++ A/sketch.js
@@ -1,13 +1,64 @@
+function star(x, y, w, c, l) {
+  let r = w / 2;
+  push();
+  translate(x, y);
+  for (let cnt = 0; cnt < l; cnt++) {
+    print(cnt);
+    line(c, -c, r, 0);
+    line(c, c, r, 0);
+    rotate(360 / l);
+  }
+  pop();
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0,0,0);
-
-  for (y = 0; y < height; y += 100) {
-    for (x = 0; x < width; x += 100) {
-      rect(x,y,90)
-    }
-  }
-
+  background(0, 0, 0);
+  angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 function draw() {}
+
+function mouseClicked() {
+  let pitch = 100;
+  let sqDim = 0.9 * pitch;
+  translate(pitch / 2, pitch / 2);
+
+  noFill();
+
+  let rn = random(0, 8);
+  print(rn);
+
+  if (rn < 2) {
+    //GENERATE RANDOM GRID OF SQUARES
+    for (y = 0; y < height; y += pitch) {
+      for (x = 0; x < width; x += pitch) {
+        let dd = random(sqDim / 8, sqDim * 2);
+        let sqc = random(["d6001a", "eb0f64", "fc1ba2"]);
+        stroke(sqc);
+        rect(x, y, dd);
+      }
+    }
+  } else if (rn < 5) {
+    // GENERATE RANDOM GRID OF STARS
+    for (y = 0; y < height; y += pitch) {
+      for (x = 0; x < width; x += pitch) {
+        let dd = random(sqDim / 8, sqDim * 2);
+        let sc = random(["#f5e400", "#f5bf1d", "#f4f568"]);
+        stroke(sc);
+        star(x, y, dd, 10, 4);
+      }
+    }
+  } else {
+    //GENERATE RANDOM GRID OF CIRCLES
+    for (y = 0; y < height; y += pitch) {
+      for (x = 0; x < width; x += pitch) {
+        let dd = random(sqDim / 8, sqDim * 2);
+        let cc = random(["1b66f7", "31a4fc", "12c0eb"]);
+        stroke(cc);
+        ellipse(x, y, dd);
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # p5.js multi sketch template
 
+NOTES TO SELF ON TROUBLES:
+Can't figure out how to make it so when squares (for example) are generated, each square can randomly be a different color rather than all the same color!!
+
+FORMAT LATER THIS IS JUST TEXT:
+
+Started by making a normal grid of squares like what we started with in class. I wanted to play with mouseClicked and instead of just regenerating the entire screen with every click I wanted each click to randomly generate either stars, circles, or squares exclusively. I couldn't think of a way to do this with the for loops still in setup, so basically everything is happening in the mouseClicked function. Instead of having one for loop that randomly generated the shapes all together, I seperated it into three for loops, one for each shape, so each mouse click could either generate a random grid of squares, circles, or stars. 
+
+Next I made it so the shapes were random sizes upon generation, then slightly more random areas of the screen, and then random colors. I wanted each shape to have its own color zone so they could still be distinguishable onscreen.
+
+Then I realized the screen was getting quite crowded after a while so I added another if/else function so that the screen would randomly reset every so often.
+
+
+
 By putting different sketch.js and index.html files in directories we can more easily switch between multiple experiments.
 
 ---
