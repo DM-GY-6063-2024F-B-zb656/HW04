fc27564d0feef05778fd2d42e74a69204ed10680
zb656 <145730190+zb656@users.noreply.github.com>
2024-10-03 22:45:45 -0400

    25 lines working?

--- B/sketch.js
+++ B/sketch.js
@@ -1,11 +1,41 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255, 255, 255);
+
+  stroke(0, 0, 0);
+
+  // original for loop i tried
+  // for (x = 0; x < width; x += 100) {
+  //   for (y = 0; y < height; y += 100) {
+  //     let r1 = random(0, width);
+  //     let r2 = random(0, height);
+  //     let r3 = random(0, width);
+  //     let r4 = random(0, height);
+
+  //     line(r1, r2, r3, r4);
+  //   }
+  // }
+
+///WHILE LOOP TEST
+
+//declare line variable
+let lines = 0;
+
+// repeat as long as lines is <= 25
+while (lines <= 24) {
+      let r1 = random(0, width);
+      let r2 = random(0, height);
+      let r3 = random(0, width);
+      let r4 = random(0, height);
+
+      line(r1, r2, r3, r4);
+  //increment by 1
+  lines += 1;
+}
 }
 
-function draw() {
-  background(255);
-  fill(0);
-  rect(width / 2 - width / 40, height / 4 - width / 20, width / 20, height / 2 + width / 10);
-  rect(width / 2.5, height / 4 + height / 2, width - 2 * width / 2.5, width / 20);
-  rect(width / 2 - width / 20 - width / 40, height / 4 - width / 20, width / 20, width / 20);
+function draw() {}
+
+function mouseClicked() {
+  setup();
 }
